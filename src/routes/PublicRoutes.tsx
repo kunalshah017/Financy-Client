9ee34af48970291329c47c8bf968f5167a246366
kunalshah017 <kunalmanishshah@gmail.com>
2024-12-11 21:07:43 +0530

    router fix

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -2,14 +2,23 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Register } from "../pages/Register";
 
 const PublicRoutes = () => {
-
     const router = createBrowserRouter([
-        { path: '/register', element: <Register /> },
+        {
+            path: "/",
+            children: [
+                {
+                    index: true,
+                    element: <Register />  // Or another component for home page
+                },
+                {
+                    path: "register",
+                    element: <Register />
+                }
+            ]
+        }
     ]);
 
-    return (
-        <RouterProvider router={router} />
-    );
+    return <RouterProvider router={router} />;
 };
 
 export default PublicRoutes;
